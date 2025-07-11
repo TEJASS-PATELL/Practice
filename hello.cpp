58a7396e56811480091014cfaf86c06f32388db0
Tejas <tejasspatell2@gmail.com>
2025-07-11 16:26:26 +0530

    tejas commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int sum(int a , int b){
+    int sum = a + b;
+    return sum;
+}
+
+int main(){
+    int a = 20;
+    int b = 40;
+    cout<<"Sum of a and b is : "<<sum(a,b);
+}
+
+
